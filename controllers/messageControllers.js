f409e5b59460649e292547b09957d2b0473d17b0
chitra0404 <chitrasuresh0404@gmail.com>
2024-06-06 19:30:28 +0530

    commit

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -5,19 +5,21 @@ const User=require('../models/userModel');
 
 module.exports.sendMessage = async (req, res) => {
   const { chatId, message } = req.body;
-  
+
   try {
     let msg = await Message.create({ sender: req.userId, message, chatId });
-    msg = await msg.populate('sender', 'name profilePic email').populate({
-      path: 'chatId',
-      select: 'chatName isGroup users',
-      model: 'Chat',
-      populate: {
-        path: 'users',
-        select: 'name email profilePic',
-        model: 'User',
-      },
-    }).execPopulate();
+
+    // Populate the fields after message creation
+    msg = await Message.findById(msg._id)
+      .populate('sender', 'name profilePic email')
+      .populate({
+        path: 'chatId',
+        select: 'chatName isGroup users',
+        populate: {
+          path: 'users',
+          select: 'name email profilePic',
+        },
+      });
 
     await Chat.findByIdAndUpdate(chatId, { latestMessage: msg });
 
@@ -28,23 +30,23 @@ module.exports.sendMessage = async (req, res) => {
   }
 };
 
-module.exports. getMessage=async(req,res)=>{
-    const {chatId}=req.params;
-    try{
-        let messages = await Message.find({ chatId })
-        .populate({
-          path: 'sender',
-          model: 'User',
-          select: 'name profilePic email',
-        })
-        .populate({
-          path: 'chatId',
-          model: 'Chat',
-        });
-  
-      res.status(200).json(messages);
-    }catch (error) {
-        res.sendStatus(500).json({ error: error });
-        console.log(error);
-      }
-}
+module.exports.getMessage = async (req, res) => {
+  const { chatId } = req.params;
+  try {
+    let messages = await Message.find({ chatId })
+      .populate({
+        path: 'sender',
+        model: 'User',
+        select: 'name profilePic email',
+      })
+      .populate({
+        path: 'chatId',
+        model: 'chat',
+      });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
